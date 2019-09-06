a3ed130101c86d98e6d50c286cd58dc7c84f9314
liuzhaohui <liuzhhb@sugon.com>
2019-09-06 23:59:59 +0800

    更新目录 增加图标仓库

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import './assets/font-awesome-4.7.0/css/font-awesome.min.css'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
